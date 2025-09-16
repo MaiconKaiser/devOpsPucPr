e2581bf5d52a990e3e4c91d49c9bf8dfd2c0cbaa
Maicon Kaiser <kaisermaicon@gmail.com>
2025-09-16 14:01:08 -0300

    Correcao

--- test/test.py
+++ test/test.py
@@ -2,17 +2,14 @@ from src.main import *;
 
 def testRoot():
     result = root();
-    yield result;
     assert result == {};
 
 def testRootSuccess():
     result = rootSuccess();
-    yield result;
     assert result == {"message": "Está funcionando!"};
 
 def testFuncaoTeste():
     result = funcaoTeste();
-    yield result;
     assert result == {
             "name": "Maicon Kaiser", 
             "idade": "28", 
